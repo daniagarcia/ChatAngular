d4402caeb0841a30315a8bacf51cc997ef7b1c00
daniagarcia <daniag.serguridad@gmail.com>
2018-07-09 14:23:58 -0500

    subiendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './Componentes/chat/chat.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes :Routes = [
+  {path:'Chat', component: ChatComponent}
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
