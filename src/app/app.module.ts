69d60806b5ee0a8e4b68a38838ba3234a6ec0012
daniagarcia <daniag.serguridad@gmail.com>
2018-07-10 14:12:12 -0500

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { ChatComponent } from './Componentes/chat/chat.component';
 import { NavbarComponent } from './Componentes/navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { LoginComponent } from './Componentes/login/login.component';
+import { NotifyModule } from 'ngx-notify';
 const routes :Routes = [
-  {path:'Chat', component: ChatComponent}
+  {path:'Chat', component: ChatComponent},
+  {path:'Login', component: LoginComponent}
 
 ];
 
@@ -16,11 +18,19 @@ const routes :Routes = [
   declarations: [
     AppComponent,
     ChatComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NotifyModule.forRoot({
+      options: { },
+      notify: {
+          progress: true
+      }
+  })
+
 
   ],
   providers: [],
